b9c568ca4341696472c7c084ea9a5cc0ee8256b0
LAPTOP-MVE52SHP\xcj <chaojian.xu@toponad.com>
2021-09-08 13:45:05 +0800

    go add 'GET' example

--- demo/go/openapi_demo.go
+++ demo/go/openapi_demo.go
@@ -14,64 +14,71 @@ import (
 )
 
 const (
-	PublisherKey = "publisher key"
+	PublisherKey = "your publisher key"
 	ContentType  = "application/json"
 )
 
 func main() {
+	// POST example:
 	reqUrl := `https://openapi.toponad.com/v1/apps`
-	reqContent := `` // request content
-
-	respData, err := dealFunc(http.MethodPost, reqUrl, reqContent)
+	reqBody := `{"limit":1}`
+	respData, err := doRequest(http.MethodPost, reqUrl, reqBody)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(respData)
+	fmt.Println("post response: ", respData)
+
+	// GET example:
+	reqUrl = `https://openapi.toponad.com/v1/waterfall/units?placement_id='xxx'`
+	respData, err = doRequest(http.MethodGet, reqUrl, "")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("get response: ", respData)
 }
 
-// request and response content
-func dealFunc(reqMethod, reqUrl, reqContent string) (string, error) {
+// doRequest requests the given reqUrl and returns the response.
+func doRequest(reqMethod, reqUrl, reqBody string) (string, error) {
 	urlInfo, err := url.Parse(reqUrl)
 	if err != nil {
 		return "", err
 	}
-
-	client := &http.Client{}
-	req, err := http.NewRequest(reqMethod, reqUrl, bytes.NewReader([]byte(reqContent)))
+	req, err := http.NewRequest(reqMethod, reqUrl, bytes.NewReader([]byte(reqBody)))
 	if err != nil {
 		return "", err
 	}
 
+	// create the MD5 value of the request body.
 	h := md5.New()
-	h.Write([]byte(reqContent))
+	h.Write([]byte(reqBody))
 	contentMD5 := hex.EncodeToString(h.Sum(nil))
 	contentMD5 = strings.ToUpper(contentMD5)
+	// create the content of common headers except the header 'X-Up-Signature'.
 	timestamp := strconv.Itoa(int(time.Now().Unix() * 1000))
 	headerString := fmt.Sprintf("X-Up-Key:%s\nX-Up-Timestamp:%s", PublisherKey, timestamp)
 
-	sign := signatureFunc(reqMethod, contentMD5, ContentType, headerString, urlInfo.Path)
+	// create the final signature.
+	sign := genSignature(reqMethod, contentMD5, ContentType, headerString, urlInfo.Path)
 	req.Header.Set("Content-Type", ContentType)
 	req.Header.Set("X-Up-Key", PublisherKey)
 	req.Header.Set("X-Up-Timestamp", timestamp)
 	req.Header.Set("X-Up-Signature", sign)
 
-	resp, err := client.Do(req)
+	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return "", err
 	}
 	defer resp.Body.Close()
-
-	fmt.Println(resp)
-
-	respByte, err := ioutil.ReadAll(resp.Body)
+	respData, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		return "", err
 	}
-	return string(respByte), nil
+	return string(respData), nil
 }
 
-func signatureFunc(httpMethod, contentMD5, contentType, headerString, urlPath string) string {
-	stringSection := []string{httpMethod, contentMD5, contentType, headerString, urlPath}
+// genSignature returns the final signature
+func genSignature(httpMethod, contentMD5, contentType, headerString, relativePath string) string {
+	stringSection := []string{httpMethod, contentMD5, contentType, headerString, relativePath}
 	stringToSign := strings.Join(stringSection, "\n")
 	h := md5.New()
 	h.Write([]byte(stringToSign))
